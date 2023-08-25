f5e47f29a0149d851cfb526ba0ad32b5e9effb1b
kenlin0109 <51696201+kenlin0109@users.noreply.github.com>
2023-08-25 14:12:18 +0800

    Initial Commit

---
+++ blood pressure/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  blood pressure
+//
+//  Created by 林秀謙 on 2023/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ blood pressure/blood_pressureApp.swift
@@ -0,0 +1,17 @@
+//
+//  blood_pressureApp.swift
+//  blood pressure
+//
+//  Created by 林秀謙 on 2023/8/25.
+//
+
+import SwiftUI
+
+@main
+struct blood_pressureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
